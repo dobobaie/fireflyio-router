3ff97e00c2bbeb7c725c2c53e8f489ce94e75954
dobobaie <dobobaie@hotmail.fr>
2020-02-23 12:51:07 +0700

    Feature(Service): update tests

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,7 +1,40 @@
 const Fireflyio = require('../../fireflyio/lib');
-const FireflyioClient = require('../../fireflyio-client/lib');
-const FireflyioRouter = require('../../fireflyio-router/lib');
+const FireflyioRouter = require('../lib');
 
 (async () => {
-  // ---
+  const app = new Fireflyio({ debug: true, allowedHttpRequests: true });
+  
+  app.extend(FireflyioRouter);
+
+  app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  });
+
+  app
+    .router
+    .focus('/users', router =>
+      router.delete('/:id/delete', ctx => {
+        console.log('/users/:id/delete', ctx);
+        ctx.body = {
+          result: true
+        };
+      })
+    )
+    .post('/login', ctx => {
+      console.log('/login', ctx);
+      ctx.body = {
+        result: true
+      };
+    })
+    .get('/hello', ctx => {
+      console.log('/hello', ctx);
+      ctx.body = {
+        message: 'Hello'
+      };
+    });
+
+  await app.listen(4000);
 })();
